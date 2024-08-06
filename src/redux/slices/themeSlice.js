8bbdc2dcaceb9a9721705a463a5900256b7dfa24
Darryl Dimas Darmawan <darryl.darmawan001@binus.ac.id>
2024-08-07 00:00:25 +0700

    Update themeSlice.js

--- src/redux/slices/themeSlice.js
+++ src/redux/slices/themeSlice.js
@@ -1,23 +1,17 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// const initialState = {
-//   darkMode: false,
-// };
+export const themeSlice = createSlice({
+  name: 'theme',
+  initialState: {
+    darkMode: false,
+  },
+  reducers: {
+    toggleDarkMode: (state) => {
+      state.darkMode = !state.darkMode;
+    },
+  },
+});
 
-// const themeSlice = createSlice({
-//   name: 'theme',
-//   initialState,
-//   reducers: {
-//     toggleDarkMode: (state) => {
-//       state.darkMode = !state.darkMode;
-//       if (state.darkMode) {
-//         document.documentElement.classList.add('dark-mode');
-//       } else {
-//         document.documentElement.classList.remove('dark-mode');
-//       }
-//     },
-//   },
-// });
+export const { toggleDarkMode } = themeSlice.actions;
 
-// export const { toggleDarkMode } = themeSlice.actions;
-// export default themeSlice.reducer;
+export default themeSlice.reducer;
